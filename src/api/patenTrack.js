5d5a81b37e7a40fd438807d62273bd95f34c6503
Vivek Kapoor <er.vivek2512@gmail.com>
2023-02-03 08:59:20 +0530

    Delete user

--- src/api/patenTrack.js
+++ src/api/patenTrack.js
@@ -555,7 +555,7 @@ class PatenTrackApi {
   }
 
   static deleteUser( ID, clientID ) {
-    return axios.delete(`${base_new_api_url}/users/${ID}`, getFormUrlHeader());  
+    return axios.delete(`${base_new_api_url}/admin/users/${clientID}/${ID}`, getFormUrlHeader());  
   }
 
   static addLawyer( user ) {
